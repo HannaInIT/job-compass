8e8508e7d9f6ea219419808dfbde43b0fe896126
Hanna Dubyna <ann10062016@gmail.com>
2026-01-18 15:12:36 +0100

    refactor: comment out unused express import and production code block

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 // Load our .env variables as early as possible so other modules that read
 // process.env at module-evaluation time receive the values.
 import "dotenv/config";
-import express from "express";
+// import express from "express";
 
 import app from "./app.js";
 import { logInfo, logError } from "./util/logging.js";
@@ -26,17 +26,17 @@ const startServer = async () => {
  * We only want to host our client code when in production mode as we then want to use the production build that is built in the dist folder.
  * When not in production, don't host the files, but the development version of the app can connect to the backend itself.
  */
-if (process.env.NODE_ENV === "production") {
-  app.use(
-    express.static(new URL("../../client/dist", import.meta.url).pathname),
-  );
-  // Redirect * requests to give the client data
-  app.get("/*file", (req, res) =>
-    res.sendFile(
-      new URL("../../client/dist/index.html", import.meta.url).pathname,
-    ),
-  );
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(
+//     express.static(new URL("../../client/dist", import.meta.url).pathname),
+//   );
+//   // Redirect * requests to give the client data
+//   app.get("/*file", (req, res) =>
+//     res.sendFile(
+//       new URL("../../client/dist/index.html", import.meta.url).pathname,
+//     ),
+//   );
+// }
 
 /****** Removed test router import and mounting. The `testRouter.js` file was deleted and is no longer used. ******/
 
